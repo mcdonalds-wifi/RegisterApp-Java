50ce7b1268adfed2f56d654b1a7ed256eadb7dd2
nlverucchi <nlverucc@uark.edu>
2020-10-11 17:43:00 -0500

    Task 11, employee detail server routing incomplete

--- src/main/java/edu/uark/registerapp/controllers/EmployeeDetailRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/EmployeeDetailRouteController.java
@@ -14,24 +14,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery;
+import edu.uark.registerapp.commands.employees.EmployeeQuery;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.Employee;
+import edu.uark.registerapp.models.api.EmployeeType;
 import edu.uark.registerapp.models.entities.ActiveUserEntity;
 
 @Controller
 @RequestMapping(value = "/employeeDetail")
 public class EmployeeDetailRouteController extends BaseRouteController {
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView start(
-		@RequestParam final Map<String, String> queryParameters,
-		final HttpServletRequest request
-	) {
+	public ModelAndView start(@RequestParam final Map<String, String> queryParameters, final HttpServletRequest request)
+	{
 
 		// TODO: Logic to determine if the user associated with the current session
 		//  is able to create an employee
-		//implement
+		final boolean active_user_status = this.activeUserExists();
+
+		if(active_user_status)
+			{ 
+				//get current user method in BaseRouteController.java
+				final Optional<ActiveUserEntity> activeUserEntity = this.getCurrentUser(request);
+				if (!activeUserEntity.isPresent()) 
+					{
+					//	return this.buildInvalidSessionResponse();
+						return this.setRedirectUrl("/mainMenu");
+					}	
+				else if (!this.isElevatedUser(activeUserEntity.get()))
+					{
+					//	return this.buildNoPermissionsResponse();
+					}
+			}
 
 		return new ModelAndView(ViewModelNames.EMPLOYEE_TYPES.getValue());
 	}
@@ -54,12 +70,31 @@ public class EmployeeDetailRouteController extends BaseRouteController {
 
 		// TODO: Query the employee details using the request route parameter
 		// TODO: Serve up the page
+
 		return new ModelAndView(ViewModelNames.EMPLOYEE_TYPES.getValue());
 	}
+
+
+	
 
 	// Helper methods
 	private boolean activeUserExists() {
 		// TODO: Helper method to determine if any active users Exist
-		return true;
+		try
+		{
+			//call method from activeEmployeeExistsQuery.java, if successful, return true, active user exists
+			this.activeEmployeeExistsQuery.execute();
+			return true;
+		} catch (final NotFoundException e) {
+			//no active user exists
+			return false;
+		}
 	}
+
+	private ModelAndView buildStartResponse(final boolean isInitialEmployee, final Map<String, String> queryParameters)
+	{
+		return this.buildStartResponse(isInitialEmployee, (new UUID(0, 0)), queryParameters);
+	}
+
+
 }
