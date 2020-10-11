3bec112c83450bdfa4070fb64523ee3e1e4a8f4d
sasonmith <33062641+sasonmith@users.noreply.github.com>
2020-10-11 17:07:55 -0500

    Update SignInRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -1,53 +1,95 @@
 package edu.uark.registerapp.controllers;
 
+import java.util.Map;
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.uark.registerapp.controllers.enums.QueryParameterNames;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import javassist.NotFoundException;
 
 @Controller
 @RequestMapping(value = "/")
 public class SignInRouteController extends BaseRouteController {
-	// TODO: Route for initial page load
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView start(
-		@RequestParam final Map<String, String> queryParameters,
-		final HttpServletRequest request
+	public ModelAndView signIn(
+		@RequestParam final Map<String, String> queryParameters
 	) {
-		
-		//if employee exists serve up sign in view/doc
-		//use functionality of task 5 ?
-		final Optional<ActiveUserEntity> activeUserEntity = this.getCurrentUser(request);
-		//check if there is not an active user and error if true
-		if (!activeUserEntity.isPresent()) {
-			return this.buildInvalidSessionResponse();
+		try {
+			this.activeEmployeeExistsQuery.execute();
+		} catch (NotFoundException e) {
+			return new ModelAndView(
+				REDIRECT_PREPEND.concat(
+					ViewNames.EMPLOYEE_DETAIL.getRoute()));
 		}
-		
-		//if there is an active user 
+
+		//create new model and view
 		ModelAndView modelAndView =
 			this.setErrorMessageFromQueryString(
 				new ModelAndView(ViewNames.SIGN_IN.getViewName()),
 				queryParameters);
 		
+		if (queryParameters.containsKey(QueryParameterNames.EMPLOYEE_ID.getValue())) {
+			modelAndView.addObject(
+				ViewModelNames.EMPLOYEE_ID.getValue(),
+				queryParameters.get(QueryParameterNames.EMPLOYEE_ID.getValue()));
+		}
+
 		return modelAndView;
+		
 	}
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public ModelAndView performSignIn(
-		// TODO: Define an object that will represent the sign in request and add it as a parameter here
-		HttpServletRequest request
-	) {
+		
+		HttpServletRequest request,
+		EmployeeSignIn ESI
+		) {
 
-		// TODO: Use the credentials provided in the request body
+		//  Use the credentials provided in the request body
 		//  and the "id" property of the (HttpServletRequest)request.getSession() variable
 		//  to sign in the user
+
+		try {
+			this.employeeSignInCommand
+				.setSessionId(request.getSession().getId())
+				.setEmployeeSignIn(ESI)
+				.execute();
+		} catch (Exception e) {
+			ModelAndView modelAndView =
+				new ModelAndView(ViewNames.SIGN_IN.getViewName());
+
+			modelAndView.addObject(
+				ViewModelNames.ERROR_MESSAGE.getValue(),
+				e.getMessage());
+			modelAndView.addObject(
+				ViewModelNames.EMPLOYEE_ID.getValue(),
+				ESI.getEmployeeId());
+
+			return modelAndView;
+		}
 
 		return new ModelAndView(
 			REDIRECT_PREPEND.concat(
 				ViewNames.MAIN_MENU.getRoute()));
 	}
+
+	//properties of class
+	@Autowired
+	private EmployeeSignInCommand employeeSignInCommand;
+
+	@Autowired
+	private ActiveEmployeeExistsQuery activeEmployeeExistsQuery;
+	
 }
