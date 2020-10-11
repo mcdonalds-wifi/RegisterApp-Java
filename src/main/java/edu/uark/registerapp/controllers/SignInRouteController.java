4771df3d98131360a790b72825f31e49d06a136d
sasonmith <33062641+sasonmith@users.noreply.github.com>
2020-10-11 16:17:13 -0500

    Update SignInRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -14,7 +14,28 @@ import edu.uark.registerapp.controllers.enums.ViewNames;
 @RequestMapping(value = "/")
 public class SignInRouteController extends BaseRouteController {
 	// TODO: Route for initial page load
-
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView start(
+		@RequestParam final Map<String, String> queryParameters,
+		final HttpServletRequest request
+	) {
+		
+		//if employee exists serve up sign in view/doc
+		//use functionality of task 5 ?
+		final Optional<ActiveUserEntity> activeUserEntity = this.getCurrentUser(request);
+		//check if there is not an active user and error if true
+		if (!activeUserEntity.isPresent()) {
+			return this.buildInvalidSessionResponse();
+		}
+		
+		//if there is an active user 
+		ModelAndView modelAndView =
+			this.setErrorMessageFromQueryString(
+				new ModelAndView(ViewNames.SIGN_IN.getViewName()),
+				queryParameters);
+		
+		return modelAndView;
+	}
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public ModelAndView performSignIn(
 		// TODO: Define an object that will represent the sign in request and add it as a parameter here
