db6449bb801a742b9a01a2d95e4b461606fb2f18
aprakash124 <70420528+aprakash124@users.noreply.github.com>
2020-10-11 18:26:08 -0500

    Add files via upload

---
+++ src/main/java/edu/uark/registerapp/commands/ActiveEmployeeExistsQuery.java
@@ -0,0 +1,21 @@
+package edu.uark.registerapp.commands.employees;
+
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ActiveEmployeeExistsQuery implements VoidCommandInterface
+{
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public void execute()
+    {
+        if (!employeeRepository.existsByIsActive(true))
+        {
+            throw new NotFoundException("Employee");
+        }
+    }
+
+}
