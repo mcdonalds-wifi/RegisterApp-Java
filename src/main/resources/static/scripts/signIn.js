7f98f51e89f47864b9b4f756d5b7f33e7d6da866
wrmendoz <70405590+wrmendoz@users.noreply.github.com>
2020-10-11 16:25:54 -0500

    Update signIn.js

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	// TODO: Anything you want to do when the page is loaded?
+	//draw attention to the first input on page load
+	const idElement=document.getElementById("employeeId");
+	idElement.focus();
+	idElement.select();
 });
-
-function validateForm() {
-	// TODO: Validate the user input
+function validateForm() { //validate inputs. calls displayError from master.js
+	const idElement=document.getElementById('employeeID');
+	const passwordElement=document.getElementById('password');
+	//id should be numeric and not blank
+	if((isNaN(idElement.value))||(idElement.value==null)) {
+		displayError("Please provide valid ID (numeric value)");
+		return false;
+	}
+	//password should be not blank
+	if(passwordElement.value==null) {
+		displayError("Please enter password");
+		return false;
+	}
 	return true;
 }
