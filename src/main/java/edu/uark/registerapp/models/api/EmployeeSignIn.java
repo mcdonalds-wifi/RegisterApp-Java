214814abf1a6d7e9e6bb3c99531b8737a636dda8
sasonmith <33062641+sasonmith@users.noreply.github.com>
2020-10-11 16:05:51 -0500

    Add files via upload

---
+++ src/main/java/edu/uark/registerapp/models/api/EmployeeSignIn.java
@@ -0,0 +1,8 @@
+package edu.uark.registerapp.models.api;
+
+public class EmployeeSignIn extends ApiResponse{
+    public EmployeeSignIn(final String employeeID, final String password){
+        //get the data from signIn.js and verify everything
+        
+    }
+}
