87ae439be3b3bde9792dc39355484995ea9786c9
sasonmith <33062641+sasonmith@users.noreply.github.com>
2020-10-11 16:06:45 -0500

    Update EmployeeSignIn.java

--- src/main/java/edu/uark/registerapp/models/api/EmployeeSignIn.java
+++ src/main/java/edu/uark/registerapp/models/api/EmployeeSignIn.java
@@ -1,8 +1,12 @@
 package edu.uark.registerapp.models.api;
 
 public class EmployeeSignIn extends ApiResponse{
-    public EmployeeSignIn(final String employeeID, final String password){
-        //get the data from signIn.js and verify everything
-        
+    public String employeeID;
+    public String password;
+
+    public EmployeeSignIn(final String eID, final String pass){
+        employeeID = eID;
+        password = pass;
     }
+
 }
