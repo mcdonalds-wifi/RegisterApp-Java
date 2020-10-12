a1d1445e954addc594233069c14c2b967fd06b04
aprakash124 <70420528+aprakash124@users.noreply.github.com>
2020-10-11 21:41:18 -0500

    Add files via upload

---
+++ src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
@@ -0,0 +1,38 @@
+package edu.uark.registerapp.commands.activeUsers;
+
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+public class ActiveUserDeleteCommand implements VoidCommandInterface
+{
+    private String sessionKey;
+
+    @Autowired
+    private ActiveUserRepository activeUserRepository;
+
+    public void setSessionKey(String s)
+    {
+        sessionKey = s;
+    }
+
+    public String getSessionKey()
+    {
+        return sessionKey;
+    }
+
+    @Transactional
+    public void execute()
+    {
+        Optional<ActiveUserEntity> activeUserEntity = activeUserRepository.findBySessionKey(sessionKey);
+
+        if (activeUserEntity.isPresent()) 
+        {
+            activeUserRepository.delete(activeUserEntity.get());
+        }
+    }
+}
+    
