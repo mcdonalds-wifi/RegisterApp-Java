4a723f8ed1066f1eca0d15749a913705f9189bfe
zltyler <43485525+zltyler@users.noreply.github.com>
2020-10-12 03:20:26 -0500

    Update ActiveUserDeleteCommand.java

--- src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
+++ src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.transaction.Transactional;
 import java.util.Optional;
 
+@service
 public class ActiveUserDeleteCommand implements VoidCommandInterface
 {
     private String sessionKey;
