cd4508b59eb3b177994b0dcb856cf6c8d2ac5a03
sasonmith <33062641+sasonmith@users.noreply.github.com>
2020-10-11 23:16:06 -0500

    Update SignInRestController.java

--- src/main/java/edu/uark/registerapp/controllers/SignInRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.uark.registerapp.commands.activeUsers.ActiveUserDeleteCommand;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.ApiResponse;
 
