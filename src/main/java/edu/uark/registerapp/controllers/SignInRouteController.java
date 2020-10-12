1eb1cc50470e1d4635cd7c2afe6b166b90f84981
aprakash124 <70420528+aprakash124@users.noreply.github.com>
2020-10-12 01:39:56 -0500

    Update SignInRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
+import edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery;
+import edu.uark.registerapp.commands.employees.EmployeeSignInCommand;
 import edu.uark.registerapp.controllers.enums.QueryParameterNames;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
@@ -63,7 +64,7 @@ public class SignInRouteController extends BaseRouteController {
 
 		try {
 			this.employeeSignInCommand
-				.setSessionId(request.getSession().getId())
+				.setSessionKey(request.getSession().getId())
 				.setEmployeeSignIn(ESI)
 				.execute();
 		} catch (Exception e) {
