78bf4701bfdba88d078f9b5716c2a574b7e46d76
zltyler <43485525+zltyler@users.noreply.github.com>
2020-10-12 03:39:04 -0500

    Update ActiveEmployeeExistsQuery.java

--- src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
+++ src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
@@ -1,5 +1,6 @@
 package edu.uark.registerapp.commands.employees;
 
+import org.springframework.stereotype.Service;
 import edu.uark.registerapp.commands.VoidCommandInterface;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
