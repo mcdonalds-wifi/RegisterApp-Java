23198ce1f2bee8fec4fd132741aaeffc275bc99b
zltyler <43485525+zltyler@users.noreply.github.com>
2020-10-11 23:32:11 -0500

    Update ProductRestController.java

--- src/main/java/edu/uark/registerapp/controllers/ProductRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductRestController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
